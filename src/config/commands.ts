ff80ed26a89b7d1a7516cddae11f4661a86baedb
Rafael do Carmo <rafaelcarmo_15@hotmail.com>
2022-04-10 22:28:10 -0300

    fix

--- src/config/commands.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-
-
-const commands = [
-    new SlashCommandBuilder()
-	.setName('hero')
-	.setDescription('Search Hero for name')
-	.addStringOption(option => option.setName('name').setDescription('The name of Hero').setRequired(true)).toJSON()
-]
--- src/lib/urql.ts
+++
@@ -1,8 +0,0 @@
-import { createClient, dedupExchange, fetchExchange, cacheExchange  } from 'urql'
-
-const client = createClient({
-    url: 'https://api-sa-east-1.graphcms.com/v2/cl1s8yyv25jmh01z7d56g8353/master',
-    exchanges: [dedupExchange, cacheExchange, fetchExchange]
-})
-
-export {client}
