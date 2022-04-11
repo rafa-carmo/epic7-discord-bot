1e7b0081d4fa8f2ab6b3619eb4c59272224ba9bf
Rafael do Carmo <rafaelcarmo_15@hotmail.com>
2022-04-11 02:46:14 -0300

    add cluster for not shutdown

--- src/bot.ts
+++ src/bot.ts
@@ -2,35 +2,45 @@ import 'dotenv/config'
 import {Client, Intents,} from "discord.js";
 import WOK from 'wokcommands'
 import path from 'path'
-
-
-let client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
-
-client.on("ready", async () => {
-    new WOK(client, {
-    commandsDir: path.join(__dirname, 'commands'),
-    typeScript: true
-})
-})
-
-client.on('shardError', error => {
-	console.error('A websocket connection encountered an error:', error);
-});
-
-client.login(process.env.TOKEN);
-
-
-
-
-//start express for heroku dont shutdown
 import express from 'express'
 
-const app = express()
-app.use(express.json())
 
-app.get('/', (_, res) => {
-    return res.sendStatus(200)   
-})
 
-app.listen(process.env.PORT || 5000, ()=>{console.log('server iniciado')})
+
+import cluster from 'cluster'
+if (cluster.isMaster) {
+
+     cluster.fork();
+   
+   cluster.on('exit', function(worker){
+      console.log('Worker ' + worker.id + ' died..');
+      cluster.fork();
+   });
+}
+else{
+
+    let client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+
+    client.on("ready", async () => {
+        new WOK(client, {
+            commandsDir: path.join(__dirname, 'commands'),
+            // typeScript: true
+        })
+    })
+
+
+    client.login(process.env.TOKEN);
+
+
+
+
+    const app = express()
+    app.use(express.json())
+
+    app.get('/', (_, res) => {
+        return res.sendStatus(200)   
+    })
+
+    app.listen(process.env.PORT || 5000, ()=>{console.log('server iniciado')})
+}
 
